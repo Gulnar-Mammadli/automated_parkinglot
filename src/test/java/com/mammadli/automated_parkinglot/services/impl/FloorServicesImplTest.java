38fd89b990eb50f4cc2de0dbccaef851b2f8de93
mammadli <gulnar.mammadli@ut.ee>
2023-02-20 21:56:57 +0200

    Add all tests

--- src/test/java/com/mammadli/automated_parkinglot/services/impl/FloorServicesImplTest.java
+++ src/test/java/com/mammadli/automated_parkinglot/services/impl/FloorServicesImplTest.java
@@ -1,7 +1,9 @@
 package com.mammadli.automated_parkinglot.services.impl;
 
 import com.mammadli.automated_parkinglot.db.dto.FloorDto;
+import com.mammadli.automated_parkinglot.db.entity.Car;
 import com.mammadli.automated_parkinglot.db.entity.Floor;
+import com.mammadli.automated_parkinglot.db.entity.ParkingLot;
 import com.mammadli.automated_parkinglot.db.repository.CarRepository;
 import com.mammadli.automated_parkinglot.db.repository.FloorRepository;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,7 +38,8 @@ class FloorServicesImplTest {
     private CarRepository carRepository;
 
 
-    String parkingLotId = "12345";
+    private static final String parkingLotId = "12345";
+    private  static final String floorId = "123";
 
     FloorDto floorDto = FloorDto.builder()
             .parkingLotId(parkingLotId)
@@ -43,6 +53,38 @@ class FloorServicesImplTest {
             .ceilingHeight(10)
             .maxCapacity(30)
             .weightCapacity(4000)
+            .build();
+
+    Floor floor1 = Floor.builder()
+            .parkingLotId(parkingLotId)
+            .ceilingHeight(20)
+            .maxCapacity(14)
+            .weightCapacity(2000)
+            .build();
+    Car car1 = Car.builder()
+            .id("12")
+            .floorId(floorId)
+            .height(30)
+            .weight(100)
+            .parkingTime(LocalDateTime.now().minusMinutes(5))
+            .unparkingTime(LocalDateTime.now())
+            .build();
+
+    Car car2 = Car.builder()
+            .id("13")
+            .floorId(floorId)
+            .height(40)
+            .weight(120)
+            .parkingTime(LocalDateTime.now().minusMinutes(7))
+            .unparkingTime(LocalDateTime.now())
+            .build();
+
+    List<Car> carsList = Arrays.asList(car1,car2);
+    List<Floor> floorList = Arrays.asList(floor,floor1);
+
+    ParkingLot parkingLot = ParkingLot.builder()
+            .id("1234567")
+            .price(BigDecimal.valueOf(5.9))
             .build();
 
     @Test
@@ -62,12 +104,56 @@ class FloorServicesImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteFloorSuccess() {
+        //Arrange
+        when(floorRepository.findById(any())).thenReturn(Optional.of(floor));
+        when(carRepository.findAllByFloorId(any())).thenReturn(carsList);
 
+        //Act
+        floorServicesImpl.delete(floorId);
+
+        //Assert
+        verify(floorRepository).findById(floorId);
+        verify(carRepository).findAllByFloorId(floorId);
 
     }
 
     @Test
-    void getFloors() {
+    void deleteFloorNotFound(){
+        //Arrange
+        when(floorRepository.findById(any())).thenReturn(Optional.empty());
+
+        //Act
+        floorServicesImpl.delete(floorId);
+
+        //Assert
+        verify(floorRepository).findById(floorId);
+    }
+
+    @Test
+    void getFloorsSuccess() {
+        //Arrange
+        when(floorRepository.findAllByParkingLotId(any())).thenReturn(floorList);
+
+        //Act
+        List<Floor> result = floorServicesImpl.getFloors(parkingLotId);
+
+        //Assert
+        assertEquals(floorList,result);
+        assertNotNull(result);
+        verify(floorRepository).findAllByParkingLotId(parkingLotId);
+    }
+
+    @Test
+    void getFloorsNotFound() {
+        //Arrange
+        when(floorRepository.findAllByParkingLotId(any())).thenReturn(Collections.emptyList());
+
+        //Act
+        List<Floor> result = floorServicesImpl.getFloors(parkingLot.getId());
+
+        //Assert
+        assertNull(result);
+        verify(floorRepository).findAllByParkingLotId(parkingLot.getId());
     }
 }
